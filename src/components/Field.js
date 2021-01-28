125d9478d680b9dd7ef65e3feb06ca9750090420
Kato <kubkate@gmail.com>
2021-01-28 09:27:18 -0800

    add comment

--- src/components/Field.js
+++ src/components/Field.js
@@ -4,6 +4,17 @@ import { Fragment } from 'react';
 function Field({ field, onChange }) {
   const {label, ...attributes} = field;
 
+  /*console.log('attributes', attributes )
+  {type: "textarea", 
+    placeholder: "Enter your message...", 
+    required: true, 
+    name: "Message", 
+    value: ""
+  }*/
+  /**
+   * Display in DOM
+   * <textarea type="textarea" placeholer="Enter your..." required name="Messgae" value>
+   */
   return (
     <Fragment>
       <label>{label}</label>
